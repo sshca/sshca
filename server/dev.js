5a0d00a63b2265ab29dcc2e2138c0a41d02147d2
Alex Lavallee <73203142+lavalleeale@users.noreply.github.com>
2023-05-28 23:15:09 -0700

    Add server hot reload

--- server/dev.js
+++ server/dev.js
@@ -1,6 +1,11 @@
 const { build } = require("esbuild");
 build({
   entryPoints: ["src/index.ts"],
+  platform: "node",
+  minify: true,
+  bundle: true,
+  outfile: "dist/server.js",
+  external: ["../node_modules/*", "./node_modules/*"],
   watch: true,
 }).catch(() => {
   process.exit(1);
