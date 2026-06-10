69ddafec058b93b5bae9a86c1d5f80b7b07adf66
Alex Lavallee <73203142+lavalleeale@users.noreply.github.com>
2026-06-10 12:15:29 -0700

    Fix Prisma server startup

--- server/src/prisma.ts
+++ server/src/prisma.ts
@@ -1,5 +1,17 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import { config } from "dotenv-safe";
+import path from "path";
 import { PrismaClient } from "./generated/index";
 
-const prisma = new PrismaClient();
+config({
+  path: path.resolve(__dirname, "../.env"),
+  example: path.resolve(__dirname, "../.env.example"),
+});
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
